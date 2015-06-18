c192f3a866fd500239099d802f9da7b5a7e89b16
Tim Brown <tim.brown@anu.edu.au>
2015-06-18 10:56:21 +1000

    Started to add file management functions

--- PhenoSnapProcessingCodeMatlab/leafBox.m
+++ PhenoSnapProcessingCodeMatlab/leafBox.m
@@ -6,7 +6,34 @@ clear all
 close all 
 clc
 
-[FileName,PathName] = uigetfile('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\2015_05_22\*.jpg','MultiSelect','on','Select the RGB image'); % set path to your images
+runMode = input('Select the appropriate response to continue:\n  (1) Select image files to process (one or many) \n  (2) Select a single folder and process all images.\n  (3) Select the parent "ExpID-Originals" folder to bulk process all files in the project\nSelect a number: ')
+if isempty(runMode)
+    runMode = 1;
+end
+
+if runMode == 1
+    [FileName,PathName] = uigetfile('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\2015_05_22\*.jpg','MultiSelect','on','Select the Experiment root folder (The folder with the EXPID name)'); % set path to your images
+elseif runMode == 2
+    [FileName,PathName] = uigetdir('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\2015_05_22\*.jpg','MultiSelect','on','Select the Experiment root folder (The folder with the EXPID name)'); % set path to your images
+elseif runMode == 3
+    expRootPath = uigetdir('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\','Select the Experiment root folder (The folder with the EXPID name)'); % set path to your images
+    expFolders = dir(expRootPath)
+    %remove non-directories (i.e. '.' and '..' folder)
+    expFolders = expFolders(arrayfun(@(expFolders) expFolders.name(1), expFolders) ~= '.')
+    %Also remove anything with an underscore at the start of the name (this is how we flag folders to omit)
+    expFolders = expFolders(arrayfun(@(expFolders) expFolders.name(1), expFolders) ~= '_')
+    return
+else
+    beep
+    disp('Response not recognized. Exiting program')
+    return
+end
+
+    
+    
+
+%[FileName,PathName] = uigetdir('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\2015_05_22\*.jpg','MultiSelect','on','Select the Experiment root folder (The folder with the EXPID name)'); % set path to your images
+[FileName,PathName] = uigetfile('X:\a_data\TimeStreams\Atkins\BVZ0047\BVZ0047-originals\2015_05_22\*.jpg','MultiSelect','on','Select the Experiment root folder (The folder with the EXPID name)'); % set path to your images
 ISC = iscell(FileName);
 if ISC == 1
     N = length(FileName);
@@ -15,12 +42,15 @@ elseif ISC == 0
     FileName = A;
     N = 1;
 end
-[num2str(N) ' images found']
+disp([num2str(N) ' images found'])
 
+folderPath = strsplit(fileparts(PathName),'\');
+expDate = folderPath(length(folderPath))
+expID = folderPath(length(folderPath)-2) %This assumes the existing folder structure is maintained
 
 for i = 1:N
-    ['Now processing image ' i ' of ' N]
-    time = tic;
+    ['Now processing image ' num2str(i) ' of ' num2str(N)]
+    tic;
     Img = im2double(imread([PathName FileName{i}]));    
     [height, width, channels] = size(Img);
     Img = imcrop(Img,[1 1 width height - 5]);
@@ -29,13 +59,14 @@ for i = 1:N
     subplot(2,1,1)
     imshow(Img)
     title('Original image')
+   disp('.')
     
     gsImg = rgb2gray(1-Img);
     
     
     edgeImg = edge(gsImg,'sobel',0.02,'both');
     edgeImg = bwmorph(edgeImg,'dilate',1);
-   
+   disp('..')
     xlines = mean(edgeImg,1);
     ylines = mean(edgeImg,2);
     [Xpks Xlocs] = findpeaks(xlines(1:800),'minpeakheight',0.1);
@@ -47,7 +78,7 @@ for i = 1:N
     Img(1:MY, 1:MX,:) = 0.8;
     Img(:,XXlocs+1399:end,:) = 0.8;
     Img(:,XXXlocs+249,:) = 0.8;
-    
+    disp('...')
     intImg = zeros(height,width,channels);
 
     intImg(:,:,1) = Img(:,:,1)./(Img(:,:,1)+Img(:,:,2)+Img(:,:,3));
@@ -66,6 +97,7 @@ for i = 1:N
     bwImg = bwareaopen(bwImg,500);
     bwImg = bwmorph(bwImg,'thin',1);
     segmentedImg = zeros(height,width,channels);
+    disp('....')
     for h = 1:height
         for w = 1:width
             if bwImg(h,w) == 1
@@ -73,7 +105,6 @@ for i = 1:N
             end
         end
     end
-            
     subplot(2,1,2)
     imshow(segmentedImg);
     title('Segmented Image');
@@ -81,33 +112,46 @@ for i = 1:N
     Stats = regionprops(bwImg,'all');
     % Need to make sure we didn't detect more than one shape
     % For now, let's grab the right-most shape and hope this selects the leaf (not rigorously tested)
+    % Actually, I'll try just grabbing the item with the alrgest area and see if that works
     % Better way to do it would be to pick the object with more green pixels
-    if length(Stats) > 1
-        
+    numObjs = length(Stats);
+    if numObjs > 1
+        disp('Multiple regions found, deleting smaller region');
+        %Delete the output with the smaller area
+        for i = 1:numObjs
+            a(i) = Stats(i).Area;
+        end
+        %Replace Stats with just the larger value
+        Stats = Stats(find(a == (max(a))));
     end
     
     
-    Results{1,1} = FileName{i};
-    Results{2,1} = 'Area';
-    Results{2,2} = Stats.Area;
-    Results{3,1} = 'Perimeter';
-    Results{3,2} = Stats.Perimeter;
-    Results{4,1} = 'Length';
-    Results{4,2} = Stats.BoundingBox(1,3);
-    Results{5,1} = 'Width';
-    Results{5,2} = Stats.BoundingBox(1,4);
-    Results{6,1} = 'Eccentricity';
-    Results{6,2} = Stats.Eccentricity;
-    Results{7,1} = 'Major Axis Length';
-    Results{7,2} = Stats.MajorAxisLength;
-    Results{8,1} = 'Minor Axis Length';
-    Results{8,2} = Stats.MinorAxisLength;
-    Results{9,1} = 'Bounding Box Area / Area';
-    Results{9,2} = Stats.Extent;
+    Results{i}{1,1} = FileName{i};
+    Results{i}{2,1} = 'Area';
+    Results{i}{2,2} = Stats.Area;
+    Results{i}{3,1} = 'Perimeter';
+    Results{i}{3,2} = Stats.Perimeter;
+    Results{i}{4,1} = 'Length';
+    Results{i}{4,2} = Stats.BoundingBox(1,3);
+    Results{i}{5,1} = 'Width';
+    Results{i}{5,2} = Stats.BoundingBox(1,4);
+    Results{i}{6,1} = 'Eccentricity';
+    Results{i}{6,2} = Stats.Eccentricity;
+    Results{i}{7,1} = 'Major Axis Length';
+    Results{i}{7,2} = Stats.MajorAxisLength;
+    Results{i}{8,1} = 'Minor Axis Length';
+    Results{i}{8,2} = Stats.MinorAxisLength;
+    Results{i}{9,1} = 'Bounding Box Area / Area';
+    Results{i}{9,2} = Stats.Extent;
 
     Name = textscan(FileName{i},'%s','delimiter','j');
-
-    xlswrite([PathName Name{1,1}{1,1} 'xlsx'], Results);
     imwrite(segmentedImg, [PathName Name{1,1}{1,1} 'SEG.png'],'png');
-    toc
+    toc/60
 end
+
+return
+%Save results
+for i = 1:N
+    xlswrite([PathName Name{1,1}{1,1} 'xlsx'], Results{i});
+end
+
